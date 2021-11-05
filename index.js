e319f59c65f1679f3a5c34923524e9d181380b78
Ashraful Islam <ashrafunit7@gmail.com>
2021-11-05 20:18:14 +0600

    dependency added

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+const { ObjectId } = require("mongodb");
+require("dotenv").config();
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+
+const uri = "mongodb+srv://devSquad:<password>@cluster0.133bl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello inventory mama! Moja hobbe!!!");
+    res.redirect('/');
+  });
+
+  app.listen(process.env.PORT || port);
